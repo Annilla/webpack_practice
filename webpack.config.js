e3dc9f15923f710996bb89f549af9868d8a09b64
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-06-30 17:00:26 +0800

    Webpack 3 Tutorial - HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: './src/index.html'
+    })
+  ]
 }
