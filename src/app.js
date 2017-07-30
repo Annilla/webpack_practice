dd77d362013fd99b409beb79258a151f759c2101
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-07-30 18:07:46 +0800

    Webpack 3 Tutorial - Style, CSS, Sass and Postcss loaders

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
-console.log('Hello World!');
+// Main CSS
+require('./app.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 
 module.exports = {
@@ -7,10 +8,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css|scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Custom template',
       template: './src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disable: false,
+      allChunks: true
     })
   ]
-}
+};
